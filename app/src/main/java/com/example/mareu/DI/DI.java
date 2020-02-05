467804a6d08a2a1985edf5bc7b681e8a7091dfca
boco0711 <s.lep.0711@gmail.com>
2020-02-06 00:41:06 +0100

    Ajout du DI

---
+++ app/src/main/java/com/example/mareu/DI/DI.java
@@ -0,0 +1,16 @@
+package com.example.mareu.DI;
+
+import com.example.mareu.service.DummyMeetingApiService;
+import com.example.mareu.service.MeetingApiService;
+
+public class DI {
+    private static MeetingApiService service = new DummyMeetingApiService();
+
+    public static MeetingApiService getMeetingApiService(){
+        return service;
+    }
+
+    public static MeetingApiService getNewInstanceApiService(){
+        return new DummyMeetingApiService();
+    }
+}
