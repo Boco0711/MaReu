0479e91a8e461b5137b560b3b249b2ff284df87a
boco0711 <s.lep.0711@gmail.com>
2020-02-13 14:29:09 +0100

    Alignement

--- app/src/main/java/com/example/mareu/service/DummyMeetingGenerator.java
+++ app/src/main/java/com/example/mareu/service/DummyMeetingGenerator.java
@@ -9,7 +9,7 @@ public class DummyMeetingGenerator {
     private static List<String> meetingParticipants = Arrays.asList("Papa", "tom", "totoooo");
 
     public static List<Meeting> DUMMY_MEETING = Arrays.asList(
-            new Meeting(1,"Salle 01", "2020/02/12","04h00", "04h59", "Mario", meetingParticipants),
+            new Meeting(1,"Salle 01", "2020/02/12", "09h00", "10h59", "Mario", meetingParticipants),
             new Meeting(2,"Salle 01", "2020/02/10", "14h50", "15h30", "Mario", meetingParticipants),
             new Meeting(3,"Salle 05", "2020/08/15", "15h00", "15h40", "Mario", meetingParticipants),
             new Meeting(4,"Salle 03", "2021/01/01", "16h00", "16h50", "Mario", meetingParticipants),
