e2965756a3bc61413fa107383efbb68f41c2dd59
boco0711 <s.lep.0711@gmail.com>
2020-02-22 01:43:00 +0100

    Indentation du code

---
+++ app/src/androidTest/java/com/example/mareu/controller/MareuInstrumentedTest.java
@@ -0,0 +1,298 @@
+package com.example.mareu.controller;
+
+import android.content.Context;
+import android.os.IBinder;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.WindowManager;
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.Root;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import com.example.mareu.R;
+import com.example.mareu.service.DummyMeetingGenerator;
+import com.example.mareu.utils.RecyclerViewItemCountAssertion;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MareuInstrumentedTest {
+
+    private int meetingInList = DummyMeetingGenerator.DUMMY_MEETING.size();
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.mareu", appContext.getPackageName());
+    }
+
+    @Test
+    public void deleteMeetingIsWorking() {
+        ViewInteraction recyclerView = onView(allOf(ViewMatchers.withId(R.id.recycler_view),
+                withParent(allOf(withId(R.id.main_activity_contenaire))),
+                isDisplayed()));
+        recyclerView.check(RecyclerViewItemCountAssertion.withItemCount(meetingInList));
+        Assert.assertTrue(meetingInList == 5);
+
+        ViewInteraction appCombatImageView = onView(allOf(withId(R.id.delete_meeting),
+                childAtPosition(allOf(withId(R.id.parent_layout),
+                        childAtPosition(withId(R.id.recycler_view),
+                                1)),
+                        3),
+                isDisplayed()));
+        appCombatImageView.perform(click());
+
+        recyclerView.check(RecyclerViewItemCountAssertion.withItemCount(meetingInList - 1));
+    }
+
+    @Test
+    public void clickOnAddMeetingButton_When_MeetingListIsShown_Then_OpenDialogBox() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.add_meeting_button),
+                        childAtPosition(
+                                allOf(withId(R.id.main_activity_contenaire),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatTextView = onView(withId(R.id.add_new_meeting_dialog_alert));
+        appCompatTextView.check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickOnAjouter_When_FieldsAreNotFilled_Then_DisplayErrorToast() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.add_meeting_button),
+                        childAtPosition(
+                                allOf(withId(R.id.main_activity_contenaire),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button1), withText("Ajouter"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton.perform(scrollTo(), click());
+
+        onView(withText(R.string.TOAST_STRING)).inRoot(isToast()).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickOnAjouterWhileFieldsAreFilled() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.add_meeting_button),
+                        childAtPosition(
+                                allOf(withId(R.id.main_activity_contenaire),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.edit_meeting_room),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                0)));
+        appCompatTextView.perform(scrollTo(), click());
+
+        DataInteraction appCompatTextView2 = onData(anything())
+                .inAdapterView(allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
+                        childAtPosition(
+                                withClassName(is("android.widget.FrameLayout")),
+                                0)))
+                .atPosition(3);
+        appCompatTextView2.perform(click());
+
+        ViewInteraction appCompatTextView3 = onView(
+                allOf(withId(R.id.edit_meeting_date),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        appCompatTextView3.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatTextView4 = onView(
+                allOf(withId(R.id.edit_meeting_start_hour),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        appCompatTextView4.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatTextView5 = onView(
+                allOf(withId(R.id.edit_meeting_end_hour),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatTextView5.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edit_meeting_subject),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                4)));
+        appCompatEditText.perform(scrollTo(), replaceText("Un Sujet de Réunion"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.edit_meeting_participant),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        appCompatEditText2.perform(scrollTo(), replaceText("DesPa"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.edit_meeting_participant), withText("DesPa"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                5)));
+        appCompatEditText3.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("Ajouter"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton4.perform(scrollTo(), click());
+
+        ViewInteraction recyclerView = onView(allOf(ViewMatchers.withId(R.id.recycler_view),
+                withParent(allOf(withId(R.id.main_activity_contenaire))),
+                isDisplayed()));
+        recyclerView.check(RecyclerViewItemCountAssertion.withItemCount(meetingInList+1));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    public static Matcher<Root> isToast() {
+        return new TypeSafeMatcher<Root>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is toast");
+            }
+
+            @Override
+            public boolean matchesSafely(Root root) {
+                int type = root.getWindowLayoutParams().get().type;
+                if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                    IBinder windowToken = root.getDecorView().getWindowToken();
+                    IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                    if (windowToken == appToken) {
+                        // windowToken == appToken means this window isn't contained by any other windows.
+                        // if it was a window for an activity, it would have TYPE_BASE_APPLICATION.
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+    }
+}
