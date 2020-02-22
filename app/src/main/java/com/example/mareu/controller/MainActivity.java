62670f01f5899e746c0455dbddac263ec59440f4
boco0711 <s.lep.0711@gmail.com>
2020-02-22 01:44:57 +0100

    Initialisation aproprié de la liste de réunion a la création de l'activité

--- app/src/main/java/com/example/mareu/controller/MainActivity.java
+++ app/src/main/java/com/example/mareu/controller/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements MeetingByRoomDial
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mApiService  = DI.getMeetingApiService();
+        mApiService  = DI.getNewInstanceApiService();
         mMeetings = mApiService.getMeetings();
         mAddMeetingButton = (ImageButton) findViewById(R.id.add_meeting_button);
         mAddMeetingButton.setOnClickListener(new View.OnClickListener() {
