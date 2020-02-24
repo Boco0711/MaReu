7747fe111f6bdcbfbd3d6a7241788da97b9f7338
boco0711 <s.lep.0711@gmail.com>
2020-02-24 13:50:48 +0100

    Changement du fonctionnement, le meeting est definis en fonction de l'id reçu

--- app/src/main/java/com/example/mareu/events/DeleteMeetingEvent.java
+++ app/src/main/java/com/example/mareu/events/DeleteMeetingEvent.java
@@ -1,12 +1,15 @@
 package com.example.mareu.events;
 
+import com.example.mareu.DI.DI;
 import com.example.mareu.model.Meeting;
+import com.example.mareu.service.MeetingApiService;
 
 public class DeleteMeetingEvent {
 
     public Meeting meeting;
 
-    public DeleteMeetingEvent(Meeting meeting) {
-        this.meeting = meeting;
+    public DeleteMeetingEvent(int i) {
+        MeetingApiService mApiService = DI.getMeetingApiService();
+        this.meeting = mApiService.getMeetingById(i);
     }
 }
